b526a516c0e9672e508837093056d72161cb7a9c
RobinGJonsson <63163226+RobinGJonsson@users.noreply.github.com>
2022-10-07 08:24:01 +0200

    update allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://boutique-ado3.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['https://boutique-ado3.herokuapp.com/',
+                 'boutique-ado3.herokuapp.com', 'localhost']
 
 
 # Application definition
